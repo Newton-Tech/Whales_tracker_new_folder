171c7add42995064edc539eb1362e299cf9ebba5
emmanuel <blessedemmachukwu2020@gmail.com>
2023-05-02 15:12:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,159 @@
+const { ethers, Contract } = require("ethers");
+// const player = require("play-sound")((opts = {}));
+
+const player = require("node-aplay");
+
+const RPC_URL = "https://cloudflare-eth.com/";
+
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
+const USDT_Contract_ADD = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+
+const ERC20_ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [{ name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "_spender", type: "address" },
+      { name: "_value", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "_from", type: "address" },
+      { name: "_to", type: "address" },
+      { name: "_value", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "_to", type: "address" },
+      { name: "_value", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      { name: "_owner", type: "address" },
+      { name: "_spender", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  { payable: true, stateMutability: "payable", type: "fallback" },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "owner", type: "address" },
+      { indexed: true, name: "spender", type: "address" },
+      { indexed: false, name: "value", type: "uint256" },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "from", type: "address" },
+      { indexed: true, name: "to", type: "address" },
+      { indexed: false, name: "value", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+];
+
+const contract = new Contract(USDT_Contract_ADD, ERC20_ABI, provider);
+
+const TRANSFER_THRESHOLD = 100000 * 10 ** 6; // 100,000 USDT wei
+
+const playSound = () => {
+  const sound = new player("/ding.mp3");
+  sound.play();
+};
+
+const main = async () => {
+  playSound();
+  const name = await contract.name();
+  console.log(
+    `Whale tracker started!\nListening for large transfers on ${name}`
+  );
+
+  contract.on("Transfer", (from, to, amount, data) => {
+    // Note: not all ERC-20 tokens index `amount`
+    // Use this instead of Ethers.js query filters
+    // https://docs.ethers.io/v5/concepts/events/
+    if (amount.toNumber() >= TRANSFER_THRESHOLD) {
+      playSound();
+      console.log(
+        `New whale transfer for ${name}: https://etherscan.io/tx/${data.transactionHash}`
+      );
+    }
+  });
+};
+
+main();
